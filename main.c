45333eec24d3cbed4fb07e485de345352c7efc69
Zijie Wu <noojeff@gmail.com>
2022-04-09 18:02:18 -0700

    Move snake

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 // Time related
 #define MILLISEC_IN_SEC 1000
 #define NANOSEC_IN_MILLISEC 1000000
-#define INTERVAL 2000
+#define INTERVAL 700
 
 // Dimension
 #define BOARD_WIDTH 15
@@ -27,23 +27,27 @@
 
 typedef unsigned long millisec_t;
 typedef struct snake_body snake_body_t;
-struct snake_body {
-    snake_body_t* next;
+struct snake_body
+{
+    snake_body_t *next;
+    snake_body_t *prev;
 
     int x;
     int y;
 };
 
-typedef enum {
+typedef enum
+{
     Right = 0,
     Left,
     Down,
     Up
 } direction_t;
 
-typedef struct {
-    snake_body_t* head;
-    snake_body_t* tail;
+typedef struct
+{
+    snake_body_t *head;
+    snake_body_t *tail;
 
     size_t length;
     direction_t direction;
@@ -52,6 +56,7 @@ typedef struct {
 ///////////////
 // Game Data
 ///////////////
+int should_exit = 0;
 // Screen buffer
 char *buffer;
 struct timespec prev_time = {0, 0};
@@ -60,32 +65,38 @@ snake_t snake;
 //////////////////////
 // Draw family APIs //
 //////////////////////
-void draw_clear_cli() {
+void draw_clear_cli()
+{
     system("clear");
 }
 
-void draw_clear_buffer() {
-    static char* buffer_empty_line = NULL;
+void draw_clear_buffer()
+{
+    static char *buffer_empty_line = NULL;
     // init once
-    if (!buffer_empty_line) {
+    if (!buffer_empty_line)
+    {
         // plus 1 because we add newline at the end
         buffer_empty_line = calloc(BUFFER_WIDTH, sizeof(char));
         memset(buffer_empty_line, ' ', BUFFER_WIDTH);
         buffer_empty_line[BUFFER_WIDTH - 1] = '\n';
     }
 
-    for (int row = 0; row < BUFFER_HEIGHT; row++) {
+    for (int row = 0; row < BUFFER_HEIGHT; row++)
+    {
         memcpy(buffer + row * BUFFER_WIDTH, buffer_empty_line, BUFFER_WIDTH);
     }
 }
 
-void draw_snake() {
+void draw_snake()
+{
     int offset_x = BOARDER_WIDTH;
     int offset_y = BOARDER_WIDTH;
 
-    snake_body_t* cur = snake.head;
+    snake_body_t *cur = snake.head;
     int head_drawn = 0;
-    while (cur != NULL) {
+    while (cur != NULL)
+    {
         int idx = (cur->x + offset_x) * BUFFER_WIDTH + (cur->y + offset_y);
         buffer[idx] = head_drawn ? SNAKE_BODY : SNAKE_HEAD;
         head_drawn = 1;
@@ -93,14 +104,16 @@ void draw_snake() {
     }
 }
 
-void draw_boarder() {
+void draw_boarder()
+{
     int offset_x = 0;
     int offset_y = 0;
     // Draw horizontal
     // Top
     offset_x = 0;
     offset_y = 1;
-    for (int y = 0; y < BOARD_WIDTH; y++) {
+    for (int y = 0; y < BOARD_WIDTH; y++)
+    {
         buffer[y + offset_y] = BOARDER_HORIZONTAL_LINE;
     }
 
@@ -108,61 +121,128 @@ void draw_boarder() {
     offset_x = 0;
     offset_y = 1;
     int x = BOARD_HEIGHT + BOARDER_WIDTH;
-    for (int y = 0; y < BOARD_WIDTH; y++) {
+    for (int y = 0; y < BOARD_WIDTH; y++)
+    {
         buffer[(x * BUFFER_WIDTH) + (y + offset_y)] = BOARDER_HORIZONTAL_LINE;
     }
 
     // Draw vertical
     // Left
     offset_x = 1;
-    for (int x = 0; x < BOARD_HEIGHT; x++) {
+    for (int x = 0; x < BOARD_HEIGHT; x++)
+    {
         buffer[(x + offset_x) * BUFFER_WIDTH] = BOARDER_VERTICAL_LINE;
     }
 
     // Right
     offset_x = 1;
     int y = BOARD_HEIGHT + BOARDER_WIDTH;
-    for (int x = 0; x < BOARD_HEIGHT; x++) {
+    for (int x = 0; x < BOARD_HEIGHT; x++)
+    {
         buffer[(x + offset_x) * BUFFER_WIDTH + y] = BOARDER_VERTICAL_LINE;
     }
 }
 
-void draw_to_buffer(int x, int y, char c) {
-
+void draw_to_buffer(int x, int y, char c)
+{
 }
 
-void draw_buffer_to_cli() {
-    for (int i = 0; i < BUFFER_HEIGHT; i++) {
+void draw_buffer_to_cli()
+{
+    for (int i = 0; i < BUFFER_HEIGHT; i++)
+    {
         fwrite(buffer + i * BUFFER_WIDTH, sizeof(char), BUFFER_WIDTH, stdout);
     }
 }
 
-void draw_string_to_ui() {
-
+void draw_string_to_ui()
+{
 }
 
 //////////////////////
 // End Draw family APIs //
 //////////////////////
 
-void update() {
+void move_snake()
+{
+    snake_body_t *old_head = snake.head;
+    snake_body_t *old_tail = snake.tail;
 
+    // Move tail to head
+    snake.tail = old_tail->prev;
+    snake.head = old_tail;
+
+    snake.tail->next = NULL;
+
+    old_tail->prev = NULL;
+    old_tail->next = old_head;
+
+    old_head->prev = snake.head;
+
+    // Update tail position
+    switch (snake.direction)
+    {
+    case Right:
+        old_tail->y = old_head->y + 1;
+        // todo: border
+        if (old_tail->y >= BOARD_WIDTH) {
+            should_exit = 1;
+        }
+        break;
+    case Left:
+        old_tail->y = old_head->y - 1;
+        if (old_tail->y < 0) {
+            should_exit = 1;
+        }
+        break;
+    case Up:
+        old_tail->x = old_head->x - 1;
+        if (old_tail->x < 0) {
+            should_exit = 1;
+        }
+        break;
+    case Down:
+        old_tail->x = old_head->x + 1;
+        if (old_tail->x >= BOARD_HEIGHT) {
+            should_exit = 1;
+        }
+        break;
+    default:
+        // Impossible
+        break;
+    }
 }
 
-void draw() {
-    // Draw to buffer
+void update()
+{
+    move_snake();
+}
+
+void draw()
+{
     draw_clear_buffer();
+
+    // Draw to buffer
     draw_snake();
     draw_boarder();
 
     // Print buffer to cli
     draw_clear_cli();
     draw_buffer_to_cli();
+
+    // debug
+    // printf("%d,%d %d,%d %d,%d\n", 
+    // snake.head->x,snake.head->y,
+    // snake.head->next->x,snake.head->next->y,
+    // snake.head->next->next->x,snake.head->next->next->y);
+    fflush(stdout);
 }
 
-void init() {
-    if ((buffer = (char *) calloc(BUFFER_WIDTH * BUFFER_HEIGHT,
-                                  sizeof(char))) == NULL) {
+void init()
+{
+    if ((buffer = (char *)calloc(BUFFER_WIDTH * BUFFER_HEIGHT,
+                                 sizeof(char))) == NULL)
+    {
         // Allocation failed
         fprintf(stderr, "Failed to allocate buffer\n");
         assert(0);
@@ -173,11 +253,18 @@ void init() {
     // Init game
     // Init snake with 3 body
     snake.direction = Right;
-    snake_body_t* body1 = (snake_body_t*)malloc(sizeof(snake_body_t));
-    snake_body_t* body2 = (snake_body_t*)malloc(sizeof(snake_body_t));
-    snake_body_t* body3 = (snake_body_t*)malloc(sizeof(snake_body_t));
+    snake_body_t *body1 = (snake_body_t *)malloc(sizeof(snake_body_t));
+    snake_body_t *body2 = (snake_body_t *)malloc(sizeof(snake_body_t));
+    snake_body_t *body3 = (snake_body_t *)malloc(sizeof(snake_body_t));
+
     body1->next = body2;
     body2->next = body3;
+    body3->next = NULL;
+
+    body3->prev = body2;
+    body2->prev = body1;
+    body1->prev = NULL;
+
     body3->x = body2->x = body1->x = BOARD_WIDTH / 2;
     body1->y = BOARD_HEIGHT / 2;
     body2->y = body1->y - 1;
@@ -189,16 +276,19 @@ void init() {
     snake.tail = body3;
 }
 
-void cleanup() {
-    if (buffer) {
+void cleanup()
+{
+    if (buffer)
+    {
         free(buffer);
         buffer = NULL;
     }
 
     // cleanup snake
-    snake_body_t* cur = snake.head;
-    while (cur != NULL) {
-        snake_body_t* tmp = cur;
+    snake_body_t *cur = snake.head;
+    while (cur != NULL)
+    {
+        snake_body_t *tmp = cur;
         cur = cur->next;
         free(tmp);
     }
@@ -206,34 +296,40 @@ void cleanup() {
     snake.head = snake.tail = NULL;
 }
 
-millisec_t to_millisec(struct timespec time) {
+millisec_t to_millisec(struct timespec time)
+{
     return time.tv_sec * MILLISEC_IN_SEC + time.tv_nsec / NANOSEC_IN_MILLISEC;
 }
 
-void to_timespec(millisec_t millisec, struct timespec *timespec) {
+void to_timespec(millisec_t millisec, struct timespec *timespec)
+{
     timespec->tv_nsec = (millisec % MILLISEC_IN_SEC) * NANOSEC_IN_MILLISEC;
     timespec->tv_sec = millisec / MILLISEC_IN_SEC;
 }
 
-millisec_t gen_wait_time(millisec_t elapsed_time) {
-    if (INTERVAL <= elapsed_time) {
+millisec_t gen_wait_time(millisec_t elapsed_time)
+{
+    if (INTERVAL <= elapsed_time)
+    {
         return 0;
     }
     return INTERVAL - elapsed_time;
 }
 
-void start_timing() {
+void start_timing()
+{
     clock_gettime(CLOCK_MONOTONIC, &prev_time);
 }
 
-void game_sleep() {
+void game_sleep()
+{
     struct timespec current_time;
     clock_gettime(CLOCK_MONOTONIC, &current_time);
 
     // 1. generate elapsed time
     millisec_t prev_milli = to_millisec(prev_time);
     millisec_t current_milli = to_millisec(current_time);
-//    printf("Elapsed time: %lu\n", elapsed_time);
+    //    printf("Elapsed time: %lu\n", elapsed_time);
 
     // 2. generate sleep time
     struct timespec req = {0, 0};
@@ -244,15 +340,13 @@ void game_sleep() {
     nanosleep(&req, NULL);
 }
 
-int main() {
+int main()
+{
     init();
-
     // main loop
-    int shouldExit = 2;
-    while (shouldExit) {
+    while (!should_exit)
+    {
         start_timing();
-
-        shouldExit--;
 
         update();
         draw();
